524f27f8ca6ce5ba1782a3cdfafe03c14dc94e89
njcalvo <njcalvo@gmail.com>
2021-03-22 09:39:21 -0300

    feat(auth): implement googleLogin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { CustomStrategy } from './strategies/custom.strategy';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly customStrategy: CustomStrategy) {}
+  async googleLogin(request: Request) {
+    const googleResponse = await this.customStrategy.validate(request);
+
+    const response = {
+      email: googleResponse.data['email'],
+      name: googleResponse.data['name'],
+    };
+
+    return response;
+  }
+}
